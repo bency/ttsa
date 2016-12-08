1d7d096f25704e0f72475f4091b879b6e21e0bcb
bency <bency@pixnet.tw>
2016-12-08 22:03:01 +0800

    修改網站名稱

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Laravel',
+    'name' => '台灣機車路權促進會',
 
     /*
     |--------------------------------------------------------------------------
