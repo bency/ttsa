9bee0aad9b0ec3ebab3c2c63b6cd16561bcd296d
bency <bency@pixnet.tw>
2017-05-29 14:10:42 +0800

    gulpfile 加上 phpunit 功能

--- gulpfile.js
+++ gulpfile.js
@@ -19,4 +19,5 @@ elixir(mix => {
     mix.copy('node_modules/d3/d3.min.js', 'public/js/d3.min.js');
     mix.copy('node_modules/c3/c3.min.js', 'public/js/c3.min.js');
     mix.copy('node_modules/c3/c3.min.css', 'public/css/c3.min.css');
+    mix.phpUnit('tests/**/*.php', 'phpunit');
 });
