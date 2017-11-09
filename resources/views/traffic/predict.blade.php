2f4b6d4c70bf319e277e07245c9bb8dfc0fe80b5
bency <bency@pixnet.tw>
2017-11-09 21:40:05 +0800

    調整縮排

--- resources/views/traffic/predict.blade.php
+++ resources/views/traffic/predict.blade.php
@@ -4,25 +4,25 @@
         var intervalId = [0, 0, 0];
         var start = function (id) {
                 let year = (new Date).getFullYear(),
-                month = (new Date).getMonth() + 1,
-                day = (new Date).getDate(),
-                phaseBegin = Date.parse(year + '/' + month + '/' + day + ' 00:00:00') / 1000 + 1;
+                    month = (new Date).getMonth() + 1,
+                    day = (new Date).getDate(),
+                    phaseBegin = Date.parse(year + '/' + month + '/' + day + ' 00:00:00') / 1000 + 1;
             intervalId[id] = setInterval(function () {
                 let green = parseInt($('#green-' + id).val()),
-                    red = parseInt($('#red-' + id).val());
-                let period = green + red;
-                let offset = parseInt($('#offset-' + id).val());
-                let now = (new Date).getTime() / 1000;
-                let phase = Math.floor((now - phaseBegin + offset) % period);
-                let progress1 = $('#progress-' + id + '-1');
-                let progress2 = $('#progress-' + id + '-2');
-                let progress3 = $('#progress-' + id + '-3');
-                let progress4 = $('#progress-' + id + '-4');
-                let width1 = (phase <= green) ? phase : 0;
-                let width2 = ((phase + red) <= period) ? red : (phase + red - period);
-                let width3 = (phase <= green) ? (green - phase) : green;
-                let width4 = ((phase + red) <= period) ? 0 : (period - phase);
-                let countDown = (width4) ? period - width1 - width2 - width3 : period - width1 - width2;
+                    red = parseInt($('#red-' + id).val()),
+                    period = green + red,
+                    offset = parseInt($('#offset-' + id).val()),
+                    now = (new Date).getTime() / 1000,
+                    phase = Math.floor((now - phaseBegin + offset) % period),
+                    progress1 = $('#progress-' + id + '-1'),
+                    progress2 = $('#progress-' + id + '-2'),
+                    progress3 = $('#progress-' + id + '-3'),
+                    progress4 = $('#progress-' + id + '-4'),
+                    width1 = (phase <= green) ? phase : 0,
+                    width2 = ((phase + red) <= period) ? red : (phase + red - period),
+                    width3 = (phase <= green) ? (green - phase) : green,
+                    width4 = ((phase + red) <= period) ? 0 : (period - phase),
+                    countDown = (width4) ? period - width1 - width2 - width3 : period - width1 - width2;
                 $('#show-red-' + id).text(red);
                 $('#show-green-' + id).text(green);
                 $('#show-offset-' + id).text(offset);
