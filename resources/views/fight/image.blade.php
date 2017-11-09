425533f8423339c9540dc725ae70c0987481dd09
bency <bency@pixnet.tw>
2017-11-10 01:54:36 +0800

    建立 drag & drop 的 view

--- resources/views/fight/image.blade.php
+++ resources/views/fight/image.blade.php
@@ -1,3 +1,13 @@
 @extends('common.base')
 @section('container')
+<article>
+    <div id="holder" class="text-center">
+        <h1>來 丟進來</h1>
+    </div>
+    <p id="upload" class="visible-xs-block"><label><input type="file"></label></p>
+    <p id="filereader">File API & FileReader API not supported</p>
+    <p id="formdata">XHR2's FormData is not supported</p>
+    <p id="progress">XHR2's upload progress isn't supported</p>
+    <p><progress id="uploadprogress" max="100" value="0">0</progress></p>
+</article>
 @endsection
