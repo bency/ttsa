b410ed17f5db43d941f3107ae798efb930be5fc5
bency <bency@pixnet.tw>
2017-12-19 23:02:31 +0800

    實作時變的倒數機制

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -35,21 +35,89 @@ var data = {
                 green: 45,
                 red: 75,
                 offset: 0,
+                countDown: 0,
+                width: [],
+            },
+            {
+                id: 4,
+                name: '基隆辛亥路口基隆路南下',
+                green: 120,
+                red: 120,
+                offset: -101,
+                countDown: 0,
+                width: [],
+            },
+            {
+                id: 5,
+                name: '基隆辛亥路口辛亥路南下',
+                green: 50,
+                red: 190,
+                offset: -45,
+                countDown: 0,
+                width: [],
+            },
+            {
+                id: 6,
+                name: '羅斯福路辛亥路路口',
+                green: 57,
+                red: 93,
+                offset: 0,
+                countDown: 0,
+                width: [],
             }
-        ]
+        ],
+        updateInterval: 500,
+        interval: []
 };
 var intersect = new Vue({
     el: '#intersect',
     data: data,
     methods: {
-        changeGreen: function (event) {
-            console.log(this.intersecions[0]);
+        changeSetting: function (id, type) {
+            this.resetInterval(id);
         },
-        resetInterval: function () {
+        resetInterval: function (id) {
             var vm = this;
-            clearInterval(this.interval);
-            this.interval = setInterval(function () {
-            }, this.updateInterval);
+            let year = (new Date).getFullYear(),
+                month = (new Date).getMonth() + 1,
+                day = (new Date).getDate(),
+                phaseBegin = Date.parse(year + '/' + month + '/' + day + ' 00:00:00') / 1000 + 1;
+            clearInterval(vm.interval[id]);
+            vm.interval[id] = setInterval(function () {
+                var green = parseInt(vm.intersecions[id].green),
+                    red = parseInt(vm.intersecions[id].red),
+                    period = green + red,
+                    offset = parseInt(vm.intersecions[id].offset),
+                    now = (new Date).getTime() / 1000,
+                    width = [],
+                    phase = Math.floor((now - phaseBegin + offset) % period);
+                width[0] = (phase <= green) ? phase : 0;
+                width[1] = ((phase + red) <= period) ? red : (phase + red - period);
+                width[2] = (phase <= green) ? (green - phase) : green;
+                width[3] = ((phase + red) <= period) ? 0 : (period - phase);
+                vm.intersecions[id].countDown = (width[3]) ? period - width[0] - width[1] - width[2] : period - width[0] - width[1];
+                $('#offset-' + id).prop('min', - parseInt(period / 2));
+                $('#offset-' + id).prop('max', parseInt(period / 2));
+                let max = 100;
+                for (let i = 3; i >= 0; i--) {
+                    if (max > width[i]) {
+                        vm.intersecions[id].width[i] = width[i];
+                        max = max - width[i];
+                    } else {
+                        vm.intersecions[id].width[i] = max;
+                        max = 0;
+                    }
+                }
+            }, vm.updateInterval);
         }
+    },
+    mounted: function () {
+        var vm = this;
+        this.$nextTick(function () {
+            let length = vm.intersecions.length;
+            for (let i = 0; i < length; i++) {
+                vm.resetInterval(vm.intersecions[i].id);
+            }
+        });
     }
 });
--- resources/views/traffic/predict.blade.php
+++ resources/views/traffic/predict.blade.php
@@ -4,36 +4,36 @@
         <div v-for="intersec in intersecions" class="col-md-12" id="form-1">
             <div class="form-inline">
                 <div class="form-group">
-                    <label>路口</label>
+                    <label class="block-label">路口</label>
                     <input class="form-control" id="location-1" data-id="1" placeholder="green" v-model="intersec.name">
                 </div>
                 <div class="form-group">
-                    <label>綠燈時間<span id="show-green-1">@{{ intersec.green }}</span></label>
-                    <input v-model="intersec.green" v-on:change="changeGreen" type="range" class="form-control period-control" v-bind:id="'green-' + intersec.id" placeholder="green" max="200" min="0" step="1">
+                    <label class="block-label">綠燈時間<span id="show-green-1">@{{ intersec.green }}</span></label>
+                    <input v-model="intersec.green" v-on:change="changeSetting(intersec.id, 'green')" type="range" class="form-control period-control" v-bind:id="'green-' + intersec.id" placeholder="green" max="200" min="0" step="1">
                 </div>
                 <div class="form-group">
-                    <label>紅燈時間<span id="show-red-1">@{{ intersec.red }}</span></label>
-                    <input v-model="intersec.red" type="range" class="form-control period-control" v-bind:id="'red-' + intersec.id" placeholder="red" max="200" min="0" step="1">
+                    <label class="block-label">紅燈時間<span id="show-red-1">@{{ intersec.red }}</span></label>
+                    <input v-model="intersec.red" v-on:change="changeSetting(intersec.id, 'red')" type="range" class="form-control period-control" v-bind:id="'red-' + intersec.id" placeholder="red" max="200" min="0" step="1">
                 </div>
                 <div class="form-group">
                     <label class="block-label">延遲<span id="show-offset-1">@{{ intersec.offset }}</span></label>
-                    <input v-model="intersec.offset" type="range" class="form-control period-control" v-bind:id="'offset-' + intersec.id" placeholder="offset" max="200" min="-200" step="1">
+                    <input v-model="intersec.offset" v-on:change="changeSetting(intersec.id, 'offset')" type="range" class="form-control period-control" v-bind:id="'offset-' + intersec.id" placeholder="offset" max="200" min="-200" step="1">
                 </div>
             </div>
             <div class="col-xs-12" id="location-label-1" style="display: none;">
-                <span id="location-input-1"></span>
+                <span>@{{ intersec.name }}</span>
             </div>
             <div class="row">
                 <div class="col-xs-10">
                     <div class="progress" data-id="1">
-                        <div class="progress-bar progress-bar-success"></div>
-                        <div class="progress-bar progress-bar-danger"></div>
-                        <div class="progress-bar progress-bar-success"></div>
-                        <div class="progress-bar progress-bar-danger"></div>
+                        <div v-bind:style="{width: intersec.width[0] + '%'}" class="progress-bar progress-bar-success"></div>
+                        <div v-bind:style="{width: intersec.width[1] + '%'}" class="progress-bar progress-bar-danger"></div>
+                        <div v-bind:style="{width: intersec.width[2] + '%'}" class="progress-bar progress-bar-success"></div>
+                        <div v-bind:style="{width: intersec.width[3] + '%'}" class="progress-bar progress-bar-danger"></div>
                     </div>
                 </div>
                 <div class="col-xs-2">
-                    <span id="countdown-1"></span>
+                    <span>@{{ intersec.countDown }}</span>
                 </div>
             </div>
         </div>
