609513eba91629892ff8ca5a3e8e0dd563253f5a
bency <bency@pixnet.tw>
2018-01-10 02:03:58 +0800

    移除新增 QA 按鈕

--- resources/views/qa/create.blade.php
+++ resources/views/qa/create.blade.php
@@ -10,7 +10,6 @@ CKEDITOR.replace('content', {
 @endsection
 @section('container')
             <h3>月經文 Q&A 設定後台</h3>
-            <a class="btn btn-success pull-right" href="{{ route('qa.create') }}">新增 QA</a>
             <form action="{{ isset($qa) ? route('qa.update', ['id' => $qa->id]) : route('qa.store') }}" method="post">
                 {{ csrf_field() }}
                 {{ isset($qa) ? method_field('PUT') : '' }}
