5740c18c0a75c0834b860e543ea00917da7a17ea
bency <bency@pixnet.tw>
2018-01-10 01:04:13 +0800

    改為月經文 Q&A

--- resources/views/showqa.blade.php
+++ resources/views/showqa.blade.php
@@ -1,6 +1,6 @@
 @extends('common.base')
 @section('container')
-            <h1>機車上國道 Q&A {{ isset($qa) ? $qa->id : 1 }}</h1>
+            <h1>月經文 Q&A {{ isset($qa) ? $qa->id : 1 }}</h1>
             <h3 class="text-center">{{ isset($qa) ? $qa->subject : '' }}</h3>
             <div class="alert alert-info">
                 <p class="text-center">{!! isset($qa) ? $qa->response : '' !!}</p>
