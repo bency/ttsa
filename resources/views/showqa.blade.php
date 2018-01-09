490cb84a2ff6526409a0a651a5d0632d801ebf04
bency <bency@pixnet.tw>
2018-01-10 01:58:57 +0800

    調整顯示 qa 的樣式

--- resources/views/showqa.blade.php
+++ resources/views/showqa.blade.php
@@ -1,8 +1,15 @@
 @extends('common.base')
 @section('container')
             <h1>月經文 Q&A {{ isset($qa) ? $qa->id : 1 }}</h1>
-            <h3 class="text-center">{{ isset($qa) ? $qa->subject : '' }}</h3>
-            <div class="alert alert-info">
-                <p class="text-center">{!! isset($qa) ? $qa->response : '' !!}</p>
-            </div>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            {{ $qa->subject }}
+            @if(Auth::check())
+            <a class="pull-right" href="{{ route('qa.edit', $qa->id) }}"><span class="glyphicon glyphicon-pencil"></span></a>
+            @endif
+        </div>
+        <div class="panel-body">
+            {!! $qa->response !!}
+        </div>
+    </div>
 @endsection
