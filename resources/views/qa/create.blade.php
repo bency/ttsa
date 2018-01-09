60d6bd21a72b4672ebe61034edfd1571548d8b76
bency <bency@pixnet.tw>
2018-01-10 02:03:29 +0800

    reword

--- resources/views/qa/create.blade.php
+++ resources/views/qa/create.blade.php
@@ -10,7 +10,7 @@ CKEDITOR.replace('content', {
     </script>
 @endsection
 @section('container')
-            <h3>機車上國道 Q&A 設定後台</h3>
+            <h3>月經文 Q&A 設定後台</h3>
             <a class="btn btn-success pull-right" href="{{ route('qa.create') }}">新增 QA</a>
             <form action="{{ isset($qa) ? route('qa.update', ['id' => $qa->id]) : route('qa.store') }}" method="post">
                 {{ csrf_field() }}
