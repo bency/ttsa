596fe7dd807b55c3c52bcdca8127a5e2d4079f20
bency <bency@pixnet.tw>
2018-01-10 00:59:53 +0800

    補上 og:type

--- resources/views/common/base.blade.php
+++ resources/views/common/base.blade.php
@@ -8,6 +8,7 @@
         <meta property="og:title" content="{{ $og_title or ''}}">
         <meta property="og:description" content="{{ $og_desc ? strip_tags($og_desc) : '' }}">
         <meta property="og:image" content="{{ $og_image or asset('/images/share_1.png') }}">
+        <meta property="og:type" content="website">
         <script>window.Laravel = {csrfToken: '{{csrf_token()}}'};</script>
 
         <title>{{ env('APP_NAME', "交通安全協會") }}</title>
