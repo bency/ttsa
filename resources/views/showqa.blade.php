44f64b955d349f9445cb95f344ded38f50a32f5b
bency <bency@pixnet.tw>
2018-01-10 01:01:13 +0800

    去除重複的 og info

--- resources/views/showqa.blade.php
+++ resources/views/showqa.blade.php
@@ -1,8 +1,4 @@
 @extends('common.base')
-@section('inhead')
-    <meta property="og:title" content="{{ isset($qa) ? $qa->subject : ''}}">
-    <meta property="og:description" content="{{ isset($qa) ? $qa->response : '' }}">
-@endsection
 @section('container')
             <h1>機車上國道 Q&A {{ isset($qa) ? $qa->id : 1 }}</h1>
             <h3 class="text-center">{{ isset($qa) ? $qa->subject : '' }}</h3>
