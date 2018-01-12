c7d3bff86deb4c8326d69ca0d6772ebf1fb661c0
bency <bency@pixnet.tw>
2018-01-13 01:48:09 +0800

    加入 report migration

---
+++ database/migrations/2018_01_04_021031_create_reports_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('pic_url');
+            $table->text('content');
+            $table->integer('company_id');
+            $table->integer('reported_at');
+            $table->integer('created_at');
+            $table->integer('updated_at');
+            $table->integer('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+}
