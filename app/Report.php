0dea9da57bea753ce454b7c8ecfca72b9f2254cf
bency <bency@pixnet.tw>
2018-01-13 01:47:30 +0800

    add Report model

---
+++ app/Report.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Report extends Model
+{
+    protected $dateFormat = 'U';
+    protected $fillable = [
+        'content',
+        'title',
+        'pic_url',
+        'reported_at',
+        'company_id',
+    ];
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+}
