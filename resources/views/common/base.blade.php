d6784c5a84af226e13b956396f97c4b86eace420
bency <bency@pixnet.tw>
2018-01-13 01:21:59 +0800

    加入報導的選單

--- resources/views/common/base.blade.php
+++ resources/views/common/base.blade.php
@@ -70,6 +70,13 @@
                             @endif
                         </ul>
                     </li>
+                    <li class="dropdown">
+                        <a href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">報導管理</a>
+                        <ul class="dropdown-menu">
+                            <li><a href="{{ route('report.index') }}">報導列表</a></li>
+                            <li><a href="{{ route('report.create') }}">新增報導</a></li>
+                        </ul>
+                    </li>
                 </ul>
               <ul class="nav navbar-nav navbar-right">
                 @if(!Auth::check())
