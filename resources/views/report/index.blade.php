02236b0d8f4e157754a7f158dc702c1528ba2046
bency <bency@pixnet.tw>
2018-01-13 02:02:03 +0800

    登入者顯示編輯按鈕

--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -7,6 +7,12 @@
             <a href="{{ route('report.show', $report->id) }}">
             {{ date('Y-m-d', $report->reported_at )}} - {{ $report->title }}
             </a>
+            @if(Auth::check())
+            <a class="label label-primary pull-right" href="{{ route('report.edit', $report->id) }}">
+                <span class="glyphicon glyphicon-pencil"></span>
+                編輯
+            </a>
+            @endif
         </li>
         @endforeach
     </ul>
